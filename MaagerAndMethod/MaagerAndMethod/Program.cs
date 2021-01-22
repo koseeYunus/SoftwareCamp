d07e0173e990d737d9a69613b3b4815c68a2ec03
koseeYunus <kose.yunus.55@gmail.com>
2021-01-23 00:58:30 +0300

    Editing.

---
+++ MaagerAndMethod/MaagerAndMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ManagerAndMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Isim = "Armut";
+            product1.Fiyati = 5;
+            product1.Aciklama = "Isparta elması.";
+            product1.StokAdedi = 1500;
+
+            Product product2 = new Product();
+            product2.Isim = "Domates";
+            product2.Fiyati = 4;
+            product2.Aciklama = "Çarşamba taze domates.";
+
+            Product[] products = new Product[] { product1, product2 };
+
+            foreach (Product p in products)
+            {
+                Console.WriteLine("İsim : " + p.Isim + "\n" + "Fiyat : " + p.Fiyati + "\n" + "Aciklama : " + p.Aciklama + "\n\n-----------------------\n");
+            }
+
+            //instance - örnek
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Add(product1);
+            sepetManager.Add(product2);
+            sepetManager.Delete(product1);
+        }
+    }
+}
---
+++ MaagerAndMethod/MaagerAndMethod/SepetManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerAndMethod
+{
+    //Manager oparasyon tutar....
+    class SepetManager
+    {
+        //Add A büyük yazılmasına naming convision denir.
+        public void Add(Product proc)
+        {
+            Console.WriteLine("Sepete " + proc.Isim + " eklendi..");
+        }
+
+        public void Delete(Product proc)
+        {
+            Console.WriteLine(proc.Isim + " ürünü silindi..");
+        }
+    }
+}
