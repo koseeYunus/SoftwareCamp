0d8fb21c9b12e97c2ad4c420c8093b76c44acbc3
koseeYunus <kose.yunus.55@gmail.com>
2021-02-02 23:12:15 +0300

    MyFinalProject first commit and push.

---
+++ MyFinalProject/DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,55 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+                new Product{ProductId=1, CategoryId=1, ProductName="Bardak", UnitPrice=1750, UnitsInStock=12},
+                new Product{ProductId=2, CategoryId=1, ProductName="Kamera", UnitPrice=75, UnitsInStock=36},
+                new Product{ProductId=3, CategoryId=2, ProductName="Telefon", UnitPrice=320, UnitsInStock=18},
+                new Product{ProductId=4, CategoryId=2, ProductName="Klavye", UnitPrice=45, UnitsInStock=88},
+                new Product{ProductId=4, CategoryId=2, ProductName="Fare", UnitPrice=45, UnitsInStock=88}
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            //LINQ : Langue Integrated Query
+            Product productToDelete=_products.SingleOrDefault(p=>p.ProductId==product.ProductId);
+            _products.Remove(productToDelete);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            //Kategoriye göre filtreleyerek uyanları yeni bir listeye atayarak geri döndürür. 
+            return _products.Where(p=>p.CategoryId==categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+    }
+}
